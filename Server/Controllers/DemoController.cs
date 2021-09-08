4e6d478a08c1badfe489937a7ef323292e1ee771
Damla Varavir <damla.varavir@sparsetechnology.com>
2021-09-08 16:13:59 +0300

    Controllers/DemoController: Unnecessary Json deleted

--- Server/Controllers/DemoController.cs
+++ Server/Controllers/DemoController.cs
@@ -34,16 +34,7 @@ namespace SparseDemo.Server.Controllers
 
         public ActionResult Index()
         {
-            //// Example JSON
-            //var json = "{\"books\":[{\"title\":\"Book 1\", \"author\":\"Author1\", \"price\":762, \"inStock\":15},{\"title\":\"Book 2\", \"author\":\"Author2\", \"price\":100, \"inStock\":1},{\"title\":\"Book 3\", \"author\":\"Author3\", \"price\":185.5, \"inStock\":5},{\"title\":\"Book 4\", \"author\":\"Author 4\", \"price\":1748, \"inStock\":3},{\"title\":\"Book 5\", \"author\":\"Author 5\", \"price\":999, \"inStock\":20},{\"title\":\"Book 6\", \"author\":\"Author 6\", \"price\":499.5, \"inStock\":3},{\"title\":\"Book 7\", \"author\":\"Author 7\", \"price\":564.5, \"inStock\":0}]}";
-            //var books = JsonConvert.DeserializeObject<Books>(json);
-            //return View(books);
-
             return View();
-      
-
-        //var json = "{[{\"Name\":\"Damla\",\"User Name\":\"admin\",\"Password\":\"admin\",\"Email\":\"admin\"},{\"Name\":\"Lale\",\"User Name\":\"user1\",\"Password\":\"0246\",\"Email\":\"hotmail\"},{\"Name\":\"Mehmet\",\"User Name\":\"user2\",\"Password\":\"1357\",\"Email\":\"gmail\"}]}";
-
-    }
+        }
     }
 }
